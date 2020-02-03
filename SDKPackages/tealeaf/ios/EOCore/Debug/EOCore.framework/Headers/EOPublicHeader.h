5192bdd0bcc5ad5990d938ba5a35e7bcb1ff7a24
Omar Hernandez <ohernandez@us.ibm.com>
2020-02-03 14:32:51 -0800

    Release build: 2.2.6
    Release Change Notes:
    Enhancements
    
    - Removed support for UIWebView due to reported Apple warnings when applications are submitted to AppStore:
      ITMS-90809: Deprecated API Usage.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.
    - arm64e bitcode slice is not created. Filed support ticket with Apple.

--- SDKPackages/tealeaf/ios/EOCore/Debug/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Debug/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.1.83"
+#define EOCORE_FRAMEWORK_VER @"2.2.6"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
