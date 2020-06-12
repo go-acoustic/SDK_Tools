963e9306a3a940db47c384d5331093fb4fcd0288
Omar Hernandez <ohernandez@us.ibm.com>
2020-06-12 15:08:34 -0700

    June 12 2020 TealeafSHA2Hash Module 1.0.4 TealeafSHA2Hash Release
    June 12 2020 TealeafSHA2Hash Module 1.0.4
    TealeafSHA2Hash Release
    Enhancements
    - None.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - None.

---
+++ SDKPackages/tealeaf/ios/Release/TealeafSHA2Hash.framework/Headers/TealeafSHA2Hash.h
@@ -0,0 +1,19 @@
+//
+//  TealeafSHA2Hash.h
+//  TealeafSHA2Hash
+//
+//  Created by Shridhar Damale on 4/13/20.
+//  Copyright © 2020 Acoustic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TealeafSHA2Hash.
+FOUNDATION_EXPORT double TealeafSHA2HashVersionNumber;
+
+//! Project version string for TealeafSHA2Hash.
+FOUNDATION_EXPORT const unsigned char TealeafSHA2HashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TealeafSHA2Hash/PublicHeader.h>
+
+
