847ec1420dec2e3b2148a9c0eb512e9c60bebecd
Omar Hernandez <omar.hernandez@acoustic.co>
2020-10-21 11:36:54 -0700

    Adjusted iOS release package to have correct dynamic builds.

---
+++ SDKPackages/tealeaf/ios/Tealeaf/Debug/Tealeaf.framework/Headers/Tealeaf.h
@@ -0,0 +1,23 @@
+//
+//  Tealeaf.h
+//  Tealeaf
+//
+//  Created by Shridhar Damale on 13/10/17.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tealeaf.
+FOUNDATION_EXPORT double TealeafVersionNumber;
+
+//! Project version string for Tealeaf.
+FOUNDATION_EXPORT const unsigned char TealeafVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tealeaf/PublicHeader.h>
+
+#import "TLFApplication.h"
+#import "TLFApplicationHelper.h"
+#import "TLFCustomEvent.h"
+#import "TLFPublicDefinitions.h"
+
