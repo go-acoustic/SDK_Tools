70cbdd16452282a9e1c02313c7fb54e27612694b
Omar Hernandez <omar.hernandez@acoustic.co>
2021-02-08 15:37:13 -0800

    February 8 2021 Tealeaf Module 10.6.42 Tealeaf Release
    February 8 2021 Tealeaf Module 10.6.42
    Tealeaf Release
    Enhancements
    
    - None.
    
    Bug Fixes
    
    - Added a flag AddGestureRecognizerUIScrollView in TealeafAdvancedConfig.json to help with not auto enabling shouldRecognizeSimultaneouslyWithGestureRecognizer on UIScrollView which breaks the layout when you have a UIPageControl with UIScrollView. When you swipe left or right, the table also scrolls due to enabling shouldRecognizeSimultaneouslyWithGestureRecognizer to return YES.
    - Removed UIWebview text that was being flagged by Apple.
    
    Known Issues
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- connectionType remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- SDKPackages/tealeaf/ios/Tealeaf/Debug/Tealeaf.framework/Headers/TLFPublicDefinitions.h
+++ SDKPackages/tealeaf/ios/Tealeaf/Debug/Tealeaf.framework/Headers/TLFPublicDefinitions.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2019 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2020 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -114,6 +114,7 @@ extern NSString* kTLFMobileStateEvent;
 #define kConfigurableItemInitialZIndex                                      @"InitialZIndex"
 #define kConfigurableItemSubViewArrayZIndexIncrementTrigger                 @"SubViewArrayZIndexIncrementTrigger"
 #define kConfigurableItemUseXpathId                                         @"UseXpathId"
+#define kConfigurableItemAddGestureRecognizerUIScrollView                   @"AddGestureRecognizerUIScrollView"
 
 // TLF Public Protocols
 @protocol TLFSavePrintScreenOperationDelegate <NSObject>
