6ab889d8e376858298417ad7b78353d82de4ec2d
Omar Hernandez <Omar.Hernandez@acoustic.co>
2021-03-19 14:47:41 -0700

    Update Tealeaf.h

--- SDKPackages/tealeaf/ios/Tealeaf/Debug/Tealeaf.framework/Headers/Tealeaf.h
+++ SDKPackages/tealeaf/ios/Tealeaf/Debug/Tealeaf.framework/Headers/Tealeaf.h
@@ -3,7 +3,14 @@
 //  Tealeaf
 //
 //  Created by Shridhar Damale on 13/10/17.
-//  Copyright © 2017 IBM. All rights reserved.
+//
+//  Copyright (C) 2016 Acoustic, L.P. All rights reserved.
+//
+//  NOTICE: This file contains material that is confidential and proprietary to
+//  Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+//  industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+//  Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+//  prohibited.
 //
 
 #import <UIKit/UIKit.h>
