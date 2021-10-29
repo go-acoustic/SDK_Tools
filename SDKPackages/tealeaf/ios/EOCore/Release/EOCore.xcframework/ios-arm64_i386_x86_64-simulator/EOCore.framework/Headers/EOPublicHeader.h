d52ef8e42a62aca4c744e12fd249a7dc19e863ca
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-10-28 18:56:36 -0700

    October 28 2021 EOCore Module 2.3.71 EOCore Release
    October 28 2021 EOCore Module 2.3.71
    EOCore Release
    Enhancements
    
    - Built with Xcode 13.1.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.68"
+#define EOCORE_FRAMEWORK_VER @"2.3.71"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
