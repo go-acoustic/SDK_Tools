f9ec5933589a08a52aa0be4505ff29a5be0c86ce
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2023-02-15 15:11:43 -0800

    February 15 2023 EOCore Module 2.3.259 EOCore Release
    February 15 2023 EOCore Module 2.3.259
    EOCore Release
    Bug Fixes
    
    - None.
    
    Improvements:
    
    - Support use case of no sim card for carriers object.
    
    Our environment for this release:
    
    - Xcode 14.2
    - MacOS 13.2
    - iOS 12.x to 16.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.257"
+#define EOCORE_FRAMEWORK_VER @"2.3.259"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
