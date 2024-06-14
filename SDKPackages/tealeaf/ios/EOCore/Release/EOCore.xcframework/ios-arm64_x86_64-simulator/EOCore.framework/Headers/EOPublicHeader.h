557266a1e0dad90ffdb304fcf1b3c66fc81f3c2b
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-06-14 10:41:55 -0700

    June 14 2024 EOCore Module 2.3.273 EOCore Release
    June 14 2024 EOCore Module 2.3.273
    EOCore Release
    Bug Fixes:
    
    - None
    
    Improvements:
    
    - Adjust for ConnectResources.bundle.
    - Add PrivacyInfo.xcprivacy and sign xcframeworks.
    
    Our environment for this release:
    
    - Xcode 15.4
    - MacOS 14.5
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.272"
+#define EOCORE_FRAMEWORK_VER @"2.3.273"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
