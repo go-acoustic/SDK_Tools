7d969363d91b35287a42b669dd14b42e5bef9817
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-11-22 19:42:23 -0800

    November 23 2024 EOCore Module 2.3.289 EOCore Release
    November 23 2024 EOCore Module 2.3.289
    EOCore Release
    Bug Fixes:
    
    - None
    
    Improvements:
    
    - When flush is executed in EOCore, it will display if payload is not empty.
    
    Our environment for this release:
    
    - Xcode 16.0
    - MacOS 15.0
    - iOS 13.x to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Release/EOCore.xcframework/ios-arm64_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.287"
+#define EOCORE_FRAMEWORK_VER @"2.3.289"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
