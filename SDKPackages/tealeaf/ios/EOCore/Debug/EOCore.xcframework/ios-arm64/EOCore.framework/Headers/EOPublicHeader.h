b1bb139012613546cc609d78b5e63ddd2c3f74ba
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-01-21 20:00:48 -0800

    January 22 2025 EOCore Module 2.3.307 EOCore Release
    January 22 2025 EOCore Module 2.3.307
    EOCore Release
    Bug Fixes:
    
    - Fix issue with xcframework signature to fix issue with validation.
    - Fix issue with Swift Package checksum.
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 16.0
    - MacOS 15.0
    - iOS 13.x to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKPackages/tealeaf/ios/EOCore/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKPackages/tealeaf/ios/EOCore/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.289"
+#define EOCORE_FRAMEWORK_VER @"2.3.307"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
